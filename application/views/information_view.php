31ce5401efeaca3057c3deb805be3080b0111cc3
LaviniaElenaLeustean <lavinia.leustean@gmail.com>
2016-11-08 14:17:01 +0100

    changed names _view.php

---
+++ application/views/information_view.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Information</title>
+</head>
+<body>
+<header>
+	<div class="container-fluid">
+		<div id="logo" class="col-xs-2"><a href="menu.html"><img src="images/tinderbox_logowhite_small.svg" alt="logo"></a></div>
+		<a href="profile.html"><div id="user" class="glyphicon glyphicon-user"></div></a>
+	</div>
+</header>
+
+	<div class="headline container-fluid">
+		<div class="row">
+			<div class="col-xs-4 nopadding">
+				<div class="dropdown">
+			  		<button onclick="toggleDropdown()" class="dropbtn"><i class="fa fa-bars" aria-hidden="true"></i></button>
+			  		<div id="myDropdown" class="dropdown-content">
+			    		<a href="schedule.html">SCHEDULE</a>
+			    		<a href="messages.html">MESSAGES</a>
+			    		<a href="qr.html">QR CODES</a>
+			    		<a href="information.html">INFO</a>
+			    		<a href="map.html">SITE MAP</a>
+			    		<a href="security.html" class="call">CALL SECURITY</a>
+			  		</div>
+				</div>
+				<button id="back-link" class="backbutton">Back</button>
+			</div>
+			<div class="col-xs-8 nopadding headline">
+				<h1>Information</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-offset-1 col-xs-11 nopadding">
+				<img class="underline" src="images/tinderbox_single_line.svg">
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div id="info">
+			<h2>Welcome to Tinderbox 2017</h2>
+			<p>On behalf of the entire team behind Tinderbox, it is a great pleasure once again to welcome you.</p>
+			<p>Whether you are participating again or for the first time, you should know that your efforts are greatly appreciated. Without you and your commitment Tinderbox could not exist.</p>
+			<p>Below you can read a short guide, which will briefly introduce you to your participation in Tinderbox.</p>
+			<p>We look forward to meeting you, and are very honored, pleased and proud that you have the time and desire to be part of our Tinderbox family.</p>
+			<p><strong>WELCOME AND HAVE A FANTASTIC FESTIVAL!</strong></p><br>
+			<div id="toc"></div>
+
+			<h2>To be a volunteer</h2>
+			<ul>
+				<li>You are a part of a team and a community of 5000 people.</li>
+				<li>You are a part of getting the wheels to go around.</li>
+				<li>As a volunteer, you have actively agreed to perform with a personal commitment, where you first and foremost must have fun, but also to deliver across Tinderbox, your team, your team leader and your area. You have to be responsible.</li>
+				<li>Learn about our Terms & Conditions, please read our FAQ and be prepared before you show up for your first shift.</li>
+				<li>FAQ: <a href="http://www.tinderbox.dk/en/volunteers/faq">www.tinderbox.dk/en/volunteers/faq</a></li>
+				<li>To be a volunteer: <a href="http://www.tinderbox.dk/en/volunteers/voluntariness-at-tinderbox">www.tinderbox.dk/en/volunteers/voluntariness-at-tinderbox</a></li>
+			</ul>
+
+			<h2>Rules of engagemenet</h2>
+			<ul>
+				<li>By signing up you agreed of our rules of engagement.</li>
+				<li>If you forgot them or just want to read them again, they are available here: <a href="http://www.tinderbox.dk/en/volunteers/rules-of-engagement">www.tinderbox.dk/en/volunteers/rules-of-engagement</a></li>
+			</ul>
+
+			<h2>Deadline for deregistration</h2>
+			<p>By the 1st of June 2016, you cannot opt out without getting in contact with your team leader. Read more in our FAQ.</p>
+
+			<h2>Shifts</h2>
+			<ul>
+				<li>You may never be absent for a shift without being in contact with your team leader.</li>
+				<li>In case of acute illness or unexpected events or otherwise you must notify your team leader as soon as possible.</li>
+				<li>You will find the contact information for your team leader in RUBY by clicking on “MY TEAMS”.</li>
+				<li>Tinderbox may in speci c cases require showing of a medical report. You are responsible for obtaining a medical report and have to cover the expenses if there are any.</li>
+			</ul>
+
+			<h2>Alcohol policy</h2>
+			<ul>
+				<li>You may not be in influenced by alcohol on your shift!</li>
+				<li>You may not drink alcohol on your shift!</li>
+			</ul>
+
+			<h2>Valuables</h2>
+			<ul>
+				<li>Valuables brought to Tinderbox are at your own risk, and our insurance does not cover damages or loss.</li>
+				<li>Read more here: <a href="http://www.tinderbox.dk/en/info">www.tinderbox.dk/en/info</a></li>
+			</ul>
+
+			<h2>Clothing</h2>
+			<ul>
+				<li>The provided t-shirt must be worn when on duty. It should not be worn when you are off.</li>
+				<li>Always make sure to bring practical clothing and footwear.</li>
+			</ul>
+
+			<h2>Meals for volunteers</h2>
+			<ul>
+				<li>All volunteers can get sandwiches during their shifts.</li>
+				<li>Before/after volunteers will have access to 4 sandwiches.</li>
+				<li>During volunteers will have access to 2 sandwiches.</li>
+				<li>Make sure you eat from home.</li>
+				<li>Volunteers are allowed to bring food, fruit and snacks.</li>
+				<li>In conjunction with the festival a kiosk will be set up, and volunteers can buy crisps, candy and chocolate.</li>
+			</ul>
+
+			<h2>Volunteer lounge</h2>
+			<p>At Tinderbox there is an area exclusively for volunteers. In the Lounge you will find:</p>
+			<ul>
+				<li>Bar</li>
+				<li>Supply of sandwiches</li>
+				<li>Hotdog stand</li>
+				<li>Kiosk</li>
+				<li>Information</li>
+				<li>Toilets</li>
+				<li>Water stations</li>
+				<li>Seating environments</li>
+				<li>...and of course a good atmosphere!</li>
+			</ul>
+
+			<h2>Wardrobe</h2>
+			<p>In the Volunteer Lounge there is a wardrobe only for volunteers. Here you can store coats and small bags. Storage is at your own risk and wardrobes are not supervised outside opening hours.</p>
+			<p>The price of the cloakroom is 10 kroner per day.</p>
+
+			<h2>Map of the site</h2>
+			<p>You can find a map of the site in the menu to the right. Here you can get an overview of:</p>
+			<ul>
+				<li>Nearest toilets</li>
+				<li>Water stations to fill up water bottles</li>
+				<li>First-Aid</li>
+				<li>Check–In.</li>
+				<li>Volunteer Lounge</li>
+			</ul>
+
+			<h2>Rosters</h2>
+			<p>Your roster will be released during May. You can find it by logging on to RUBY and selecting MY SHIFTS</p>
+
+			<h2>Check-In</h2>
+			<ul>
+				<li>NOTICE! New location. This year the Check-In for volunteers is placed in connection with the main entrance, which is located by Falen. Orientate yourself in this guide.</li>
+				<li>Before you can get onto the site, you have to go through our Check-In, where you must be wearing the wristband, that gives you access to the festival site.</li>
+				<li>To check-in, you have to bring your ticket and picture-ID!</li>
+				<li>You will find your ticket by logging on to RUBY and selecting MY TICKET.</li>
+				<li>You can bring your ticket on smartphone or tablet.</li>
+				<li>Check-In is open during the construction days, during and after Tinderbox - see opening hours below.</li>
+				<li>If you have a shift before Check-In opens, you will arrange a meeting point with your team leader, and he/she will make sure that you get a temporary wristband.</li>
+				<li>AVOID QUEUES!!! Get your bracelet on before Tinderbox. That way you will avoid possible queues at the Check-In in the busy periods.</li>	
+			<table class="hours-table">
+				<tr>
+					<th><li><h5>OPENING HOURS:</h5></li></th>
+				</tr>
+				<tr>
+					<td>June 19 - 21:</td>
+					<td>08.00 - 20.00</td>
+				</tr>
+				<tr>
+					<td>June 22:</td>
+					<td>07.00 - 20.00</td>
+				</tr>
+				<tr>
+					<td>June 23:</td>
+					<td>07.00 - 24.00</td>
+				</tr>
+				<tr>
+					<td>June 24:</td>
+					<td>08.00 - 24.00</td>
+				</tr>
+				<tr>
+					<td>June 25:</td>
+					<td>08.00 - 20.00</td>
+				</tr>
+				<tr>
+					<td>June 26:</td>
+					<td>09.00 - 12.00</td>
+				</tr>
+			</table>
+
+			<li><h5>ADDRESS:</h5>
+				<p>Tusindårsskoven, Falen, 5200 Odense V (By the main entrance)</p>
+			</li>
+			</ul>
+
+			<h2>Safety</h2>
+			<ul>
+				<li>At Tinderbox there has been established a team consisting of professional security guards and well-trained first aiders. Always follow references from security personnel.</li>
+				<li>At the concert site, DO NOT contact 112! Contact the security sta  on site. This is done through radio channel 1 or 16. There are radios in all ports, stalls and bars.</li>
+				<li>In case of fire you must immediately leave the site and contact security personnel via radio. Then you may try to extinguish the fire, if it’s possible and safe.</li>
+				<li>Do you, a colleague or a guest require first aiders, you should refer to the first-aiders station. Is it acute first aiders can be called via radio channel 1 or 16.</li>
+				<li>Fights, threats and similar is not accepted in any way. If you see or get involved in this you need to call the guards. Always remember to keep well away from a fight and let the guards handle the situation.</li>
+				<li>At Tinderbox work environment and safety for our volunteers is very important. We have made several initiatives to make the safety of our volunteers as high as possible. However, we also encourage everyone to consider their own and others’ safety. Use common sense on your own and others’ behalf, keep an eye on what is going on around you, so you can take care of yourself and your colleagues.</li>
+			</ul>
+
+			<h2>Contact information</h2>
+			<p>Tinderbox is held in Tusindårsskoven, Falen, 5200 Odense V</p>
+			<p>Volunteer: <a href="mailto:frivillig@tinderbox.dk">frivillig@tinderbox.dk</a></p>
+			<p>RUBY support: <a href="techsupport@tinderbox.dk">techsupport@tinderbox.dk</a></p>
+			<p>RUBY: <a hreF="http://ruby.tinderbox.dk">www.ruby.tinderbox.dk</a></p>
+			<p>Facebook: <a href="http://facebook.com/tinderboxers/">www.facebook.com/tinderboxers</a></p>
+			<p>Accommodation: <a href="http://tinderbox.dk/overnatning/">www.tinderbox.dk/overnatning</a></p>
+			<p>Accommodation (Facebook): <a href=http://facebook.com/events/508759709291144/>www.facebook.com/events/508759709291144</a></p>
+		</div>
+	</div>
+
+
+<footer>
+	<div class="footer">
+		<div class="social-media">
+			<span>Follow us</span>
+			<div class="social-media-icons">
+				<i class="fa fa-facebook-official" aria-hidden="true"></i>
+				<i class="fa fa-instagram" aria-hidden="true"></i>
+				<i class="fa fa-twitter" aria-hidden="true"></i>
+				<i class="fa fa-spotify" aria-hidden="true"></i>
+			</div>
+		</div>
+		<div class="tuborg">
+			<img src="images/tuborg.svg">
+		</div>
+	</div>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="js/scripts.js"></script>
+
+<script type="text/javascript">
+$( document ).ready(function() {
+	$("#toc").append('<h2>Table of contents</h2><ul></ul>');
+	$("h2").each(function(i) {
+	    var current = $(this);
+	    current.attr("id", "title" + i);
+	    $("#toc ul").append("<li><a id='link" + i + "' href='#title" +
+	        i + "' title='" + current.attr("tagName") + "'>" + 
+	        current.html() + "</a></li>");
+	});
+});
+</script>
+</body>
+</html>
