1599c6b412fca59d4fc1b490bb45085e42b66b14
LaviniaElenaLeustean <lavinia.leustean@gmail.com>
2016-11-16 23:16:12 +0100

    qr not done, errors in database

--- application/controllers/qrcodes.php
+++ application/controllers/qrcodes.php
@@ -3,6 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Qrcodes extends CI_Controller {
 
+function __Construct(){
+	parent::__Construct();
+	$this->load->database();
+	$db = $this->load->database();
+	$query = $db->get('users');  
+
+	var_dump($query);
+
+}
+
 public function index()
 	{
 		$this->load->helper('url');
@@ -11,3 +21,5 @@ public function index()
 		$this->load->view('footer_view');
 	}
 }
+
+  
