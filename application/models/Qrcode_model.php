38b4082810ac25297270d2e2723bc5e444d66dd1
ingibjorghil <ingibjorg1981@gmail.com>
2016-11-27 15:31:21 +0100

    Revert "Merge branch 'master' into Ingibjörg"
    This reverts commit 7681aa75e7352ab434f2642e82540733b8589c05, reversing
    changes made to ddadb9f7230d4f163535d326fe2627f04ab1be19.

--- application/models/Qrcode_model.php
+++ application/models/Qrcode_model.php
@@ -6,11 +6,11 @@ class Qrcode_model extends CI_Model {
         parent::__construct();
     }
 	function insert_data($data) {
-		$this->db->insert('qruser', $data);
+		$this->db->insert('users', $data);
 	}
 
 	function get_qr($id) {
-		$query = sprintf('SELECT * FROM qruser
+		$query = sprintf('SELECT * FROM users
 			WHERE user_id = "%d"'
 			, $id);
 
@@ -21,7 +21,7 @@ class Qrcode_model extends CI_Model {
 
 	function check_bool()
 		{ 	
-			$query = sprintf('UPDATE qruser SET bool = NOT(bool)');
+			$query = sprintf('UPDATE users SET bool = NOT(bool)');
 			$result = $this->db->query($query);
 			return $result->result();
 		}
