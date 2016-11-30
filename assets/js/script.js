14c45ba1e0261cc3bf55030b3c79ac69f1f45b25
Eelpan <nikolaj__moller@hotmail.com>
2016-12-01 00:10:23 +0100

    Upload img working
    Upload image to registration page.
    js file for ajax
    php file for backend storing on server
    + added html that the ajax can react on

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,36 @@
-jQuery(document).ready(function (e) {
- jQuery('#form').on('submit',(function(e) {
-  e.preventDefault();
-  jQuery.ajax({
-    url: 'http://localhost:8888/profile/upload_image',
-    type: 'POST',
-    data:  new FormData(this),
-    contentType: false,
-    cache: false,
-    processData:false,
-    beforeSend : function()
-   {
-    //$("#preview").fadeOut();
-    jQuery('#err').fadeOut();
-   },
-   success: function(data)
+$(document).ready(function (e) {
+  $("#form").on('submit',(function(e) {
+    e.preventDefault();
+    $.ajax({
+          url: 'http://localhost:8888/tinderbox/tinderbox/ajaxupload',
+      type: "POST",
+      data:  new FormData(this),
+      contentType: false,
+          cache: false,
+      processData:false,
+      beforeSend : function()
       {
-    if(data =='invalid file')
-    {
-     // invalid file format.
-     jQuery('#err').html('Invalid File !').fadeIn();
-    }
-    else
-    {
-     // view uploaded file.
-     jQuery('#preview').html(data);
-     jQuery('#form')[0].reset(); 
-     console.log('Success');
-    }
+        //$("#preview").fadeOut();
+        $("#err").fadeOut();
       },
-     error: function(e) 
-      {
-    jQuery('#err').html(e).fadeIn();
-      }          
-    });
- }));
+      success: function(data)
+        {
+        if(data=='invalid')
+        {
+          // invalid file format.
+          $("#err").html("Invalid File !").fadeIn();
+        }
+        else
+        {
+          // view uploaded file.
+          $("#preview").html(data).fadeIn();
+          $("#form")[0].reset();  
+        }
+        },
+        error: function(e) 
+        {
+        $("#err").html(e).fadeIn();
+        }           
+     });
+  }));
 });
